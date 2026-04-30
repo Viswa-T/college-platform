a310ec78f7ea5c8a85344d789f2e76d793a2335c
Viswa-T <viswamalaravan@gmail.com>
2026-04-30 18:56:23 +0530

    Update page.tsx

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -7,13 +7,13 @@ export default function LoginPage() {
   const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
-    const res = await fetch("http://localhost:5000/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-    });
+    const res = await fetch("https://college-platform-j7qn.onrender.com/login", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({ email, password }),
+});
 
     const data = await res.json();
     alert(data.success ? "Login Success" : "Invalid Credentials");
