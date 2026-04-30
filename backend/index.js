dd270cc42e316f8f6fbd726473dd0bbd28177987
Viswa-T <viswamalaravan@gmail.com>
2026-04-30 18:44:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -145,6 +145,8 @@ app.get("/predict", (req, res) => {
 });
 
 /* ===================== SERVER ===================== */
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
